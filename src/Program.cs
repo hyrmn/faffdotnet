f7bfd6a3024024d1bf0bb9efef2de3efbcb97c9b
Ben Hyrman <ben@hyr.mn>
2021-09-01 09:42:25 -0400

    Re-fix the build

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,11 @@
+using LiteDB;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages();
+builder.Services
+    .AddSingleton<TodoRepository>()
+    .AddRazorPages();
+
+builder.Logging.AddConsole().SetMinimumLevel(LogLevel.Warning);
 
 var app = builder.Build();
 
@@ -13,9 +19,55 @@ else
 }
 
 app.UseStatusCodePagesWithReExecute("/Error", "?code={0}");
-
 app.UseStaticFiles();
-app.UseAuthorization();
+
+app.MapGet("/favicon.ico", () => Results.NotFound());
+app.MapGet("/api/todo", () => new Todo(ObjectId.NewObjectId(), "Test", false));
+
 app.MapRazorPages();
 
 app.Run();
+
+record Todo(ObjectId? Id, string Title, bool Completed = false, int? Order = null);
+
+record Summary(int Total, int Start, Todo[] List);
+
+class TodoRepository
+{
+    private readonly string dbPath;
+    private readonly ILogger<TodoRepository> logger;
+
+    public TodoRepository(IWebHostEnvironment env, ILogger<TodoRepository> logger)
+    {
+        this.dbPath = Path.Combine(env.ContentRootPath, "data/todos.db");
+        this.logger = logger;
+    }
+
+    public Summary Paged(int start, int pageSize)
+    {
+        using var db = new LiteDatabase(dbPath);
+        var col = db.GetCollection<Todo>(nameof(Todo));
+        col.EnsureIndex(s => s.Order);
+
+        return new(
+            Total: col.Count(),
+            Start: start,
+            List: col.Find(Query.All(Query.Descending), skip: start, limit: pageSize).ToArray()
+        );
+    }
+
+    public Todo Get(ObjectId id)
+    {
+        using var db = new LiteDatabase(dbPath);
+        var col = db.GetCollection<Todo>(nameof(Todo));
+        return col.FindById(id);
+    }
+
+    public void Save(Todo todo)
+    {
+        using var db = new LiteDatabase(dbPath);
+        var col = db.GetCollection<Todo>(nameof(Todo));
+        col.EnsureIndex(s => s.Order);
+        col.Upsert(todo);
+    }
+}
